226c56d0bfb1ee2ac620f9072eb4d4b63499c5c7
Graham Brereton <prograhamer@users.noreply.github.com>
2023-12-06 17:33:29 -0500

    Add eagle support, refactor JS logic

--- elephant.js
+++ elephant.js
@@ -1,32 +1,43 @@
-let getURL;
-if (typeof chrome !== 'undefined') {
-	getURL = chrome.runtime.getURL;
-} else {
-	getURL = browser.runtime.getURL;
+const getURL = (asset) => {
+	if (typeof chrome !== 'undefined') {
+		return chrome.runtime.getURL(asset);
+	}
+
+	return browser.runtime.getURL(asset);
 }
 
-const url = getURL("assets/elephant.mp3");
-const trumpeter = document.createElement("audio");
-const source = document.createElement("source");
-source.src = url;
-source.type = "audio/mpeg";
-trumpeter.appendChild(source);
+const getAudioElement = (asset) => {
+	const url = getURL(asset);
+	const audio = document.createElement("audio");
+	const source = document.createElement("source");
+	source.src = url;
+	source.type = "audio/mpeg";
+	audio.appendChild(source);
+	return audio;
+}
 
+const trumpeter = getAudioElement("assets/elephant.mp3");
 document.body.append(trumpeter);
+const chitterer = getAudioElement("assets/eagle.mp3");
+document.body.append(chitterer);
 
 let logger = (records, observer) => {
-  for (const record of records) {
-    for (const added of record.addedNodes) {
-			if (added.classList && added.classList.contains("ark-card-title") && /(african bush|asian) elephant/i.test(added.innerHTML)) {
-				trumpeter.play();
+	for (const record of records) {
+		for (const added of record.addedNodes) {
+			if (added.classList && added.classList.contains("ark-card") && added.classList.contains("animal-card")) {
+				if (/resistance/i.test(added.innerText)) {
+					trumpeter.play();
+				} else if (/determination/i.test(added.innerText)) {
+					chitterer.play();
+				}
 			}
-    }
-  }
+		}
+	}
 };
 
 const observerOptions = {
-  childList: true,
-  subtree: true,
+	childList: true,
+	subtree: true,
 };
 
 const observer = new MutationObserver(logger);
