c74a62c16e912ccd6dd3c78dc843ce46c59cb9d3
Graham Brereton <prograhamer@users.noreply.github.com>
2023-12-12 22:26:04 -0500

    Rhino whomp whomp

--- elephant.js
+++ elephant.js
@@ -7,28 +7,44 @@ const getURL = (asset) => {
 }
 
 const getAudioElement = (asset) => {
-	const url = getURL(asset);
+	const url = getURL(`assets/${asset}.mp3`);
 	const audio = document.createElement("audio");
 	const source = document.createElement("source");
+	audio.className = `elephant-spy ${asset}`;
 	source.src = url;
 	source.type = "audio/mpeg";
 	audio.appendChild(source);
+	document.body.append(audio);
 	return audio;
 }
 
-const trumpeter = getAudioElement("assets/elephant.mp3");
-document.body.append(trumpeter);
-const chitterer = getAudioElement("assets/eagle.mp3");
-document.body.append(chitterer);
+const alerts = [
+	{
+		re: /resistance/i,
+		audio: getAudioElement('elephant'),
+	},
+	{
+		re: /determination/i,
+		audio: getAudioElement('eagle'),
+	},
+	{
+		re: /assertion/i,
+		audio: getAudioElement('rhino'),
+	},
+];
 
 let logger = (records, observer) => {
 	for (const record of records) {
 		for (const added of record.addedNodes) {
 			if (added.classList && added.classList.contains("ark-card") && added.classList.contains("animal-card")) {
-				if (/resistance/i.test(added.innerText)) {
-					trumpeter.play();
-				} else if (/determination/i.test(added.innerText)) {
-					chitterer.play();
+				for (alert of alerts) {
+					if (alert.re.test(added.innerText)) {
+						alert.audio.play().then(() => {
+							console.log("played audio for alert", alert);
+						}).catch((e) => {
+							console.log("failed to play audio", e);
+						});
+					}
 				}
 			}
 		}
